777526b03a14bca897a6d28b43152e39268ad3b2
Maria Loriana Porumb <e1429844@student.tuwien.ac.at>
2020-12-26 16:02:26 +0100

    Add function to show changes for each file, and function to retrieve sha of a commit by name

--- get_commit_details.js
+++ get_commit_details.js
@@ -38,6 +38,7 @@ async function get_next_commit(current_sha) {
     const next_commit = `git log --reverse --pretty=%H master | grep -A 1 $(git rev-parse ${current_sha}) | tail -n1`
     const access_repo = `cd ${local}`
 
+    /*TODO: make sure to move onto the next next commit in case the next one is a theory commit*/
     const next_commit_command = await exec(access_repo + " && " + next_commit)
     const next_commit_sha = next_commit_command.stdout
 
@@ -53,11 +54,68 @@ async function get_diff(commit_sha) {
     const commit = await repo.getCommit(commit_sha)
     console.log(commit.message())
     const diff_array = await commit.getDiff()
-    console.log(diff_array)
+
+    const diff_files = await get_diff_for_files(diff_array)
+
+    /*TODO: return some object here */
+
+    diff_files.forEach(function(file) {
+      console.log("============================= NEW FILE =============================")
+      console.log(file)
+    })
+      
 }
 
 
 
+async function get_diff_for_files(diff_array) {
+  var diffFiles = []
+
+  var i;
+  for (i=0; i<diff_array.length; i++) { 
+    var patches = await diff_array[i].patches()
+    
+    var j;
+    for (j=0; j<patches.length; j++) {
+      var hunks = await patches[j].hunks()
+      var diffFile = ''
+
+      var k;
+      for (k=0; k<hunks.length; k++) {
+        var lines = await hunks[k].lines()
+
+        var l;
+        for (l=0; l<lines.length; l++) {
+          diffFile += String.fromCharCode(lines[l].origin()) +
+            lines[l].content().trim() + '\n'
+        }
+      }
+
+      diffFiles.push(diffFile)
+    }
+
+    
+  }
+
+  return diffFiles;
+    
+}
+
+
+
+async function get_theory_commit_sha(commit_sha) {
+  //here we'll insert a pattern instead (aka:THEORY#some_sha#), as soon as we have a tryout repo
+  const search_by_message = `git log --all --grep=${commit_sha}`
+  const access_repo = `cd ${local}`
+
+  const theory_command = await exec(access_repo + " && " + search_by_message)
+  const theory_sha = theory_command.stdout
+
+  console.log(theory_sha)
+}
+
+
+/*
 async function get_all_commits_sha() {
     const repo = await nodegit.Repository.open(local)
   
@@ -70,9 +128,6 @@ async function get_all_commits_sha() {
       hist.on("error", reject);
     });
   
-    /**
-     * this part here is important in case you want to get the commits in increasing chronological order (oldest first)
-     */
     commits.reverse()
   
     for (var i = 0; i < commits.length; i++) {
@@ -83,11 +138,14 @@ async function get_all_commits_sha() {
         
       console.log(sha + " " + msg);
       var diff = await commits[i].getDiff()
-      console.log(await diff[0].patches())
+      
      
     }
   
   }
+*/
+
+ 
   
 
 
@@ -96,4 +154,6 @@ async function get_all_commits_sha() {
 /*get_first_commit().then(console.log("DOONE"))
 get_next_commit("35949ff7dd29c197171339ae6a51389b30787c8f").then(console.log("DOONE"))*/
 //get_diff('13a435e480e9ced68a06414d65589d7b2fe90964').then(console.log("DOONE"))
-get_all_commits_sha().then(console.log('Done'))
+//get_diff('12c4e858812fa47eed16fcd689708a1f9bc75555').then(console.log("DOONE"))
+//get_all_commits_sha().then(console.log('Done'))
+get_theory_commit_sha("Introducing").then(console.log("GOT SHA"))
